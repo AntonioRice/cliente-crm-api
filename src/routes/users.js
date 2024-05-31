53b65e06b106f841c89d8bd530d4d1d23be3a0f5
Antonio Rice <ricex407@gmail.com>
2024-05-31 16:04:58 -0500

    auth to createUser

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 const { createUser, completeUserRegistration } = require("../controllers/usersController");
 
-router.route("/users").post(createUser);
+router.route("/users").post(isAuthenticatedUser, authorizeRoles(["SuperAdmin", "Admin"]), createUser);
 router.route("/complete-registration").put(completeUserRegistration);
 
 module.exports = router;
