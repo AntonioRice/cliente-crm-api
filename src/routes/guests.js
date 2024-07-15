ed1f20a8ae63aa56426ae0f24007fb3e104e8349
AntonioRice <ricex407@gmail.com>
2024-07-15 11:38:47 -0500

    ...

--- src/routes/guests.js
+++ src/routes/guests.js
@@ -11,9 +11,9 @@ const {
   deleteGuest,
 } = require("../controllers/guestsController");
 
-router.route("/guests").get(isAuthenticatedUser, getAllGuests).post(isAuthenticatedUser, createGuest);
 router.route("/guests/search").get(isAuthenticatedUser, searchGuests);
 router.route("/guests/current").get(isAuthenticatedUser, getCurrentGuests);
 router.route("/guests/:id").get(isAuthenticatedUser, getGuestById).delete(isAuthenticatedUser, deleteGuest);
+router.route("/guests").get(isAuthenticatedUser, getAllGuests).post(isAuthenticatedUser, createGuest);
 
 module.exports = router;
